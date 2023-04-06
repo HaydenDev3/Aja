192d2a2e74392ed5fd51351b8e4eeabf3b1335fc
Unbreakablenight <unbreakableplayz23@gmail.com>
2023-04-06 14:58:32 +1000

    🚀 - Added ParticleJs, Hopefully it works, Added Pages

--- server.ts
+++ server.ts
@@ -4,12 +4,28 @@ dotenv.config();
 import express, { Application, Request, Response } from 'express';
 import { createServer } from 'http';
 import { router as CommandsRouter } from './routes/commands';
+import config from "./config.json";
+import { load } from 'particles.js';
+import path from "path";
 
 const app = express();
 export const server = createServer(app);
 
+app.use(express.static(path.join(__dirname,'public')));
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+load('particles-js', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
 app.get('/', (req: Request, res: Response) => 
-    res.status(200).send("Hello World"));
+    res.status(200).sendFile(path.join(__dirname, "public", "index.html")));
+
+app.get('/tos', (req: Request, res: Response) => 
+    res.status(200).sendFile(path.join(__dirname, "public", "Terms_Of_Service.html")));
+
+app.get('/privacy', (req: Request, res: Response) => 
+    res.status(200).sendFile(path.join(__dirname, "public", "Privacy_Policy.html")));
 
 app.use('/commands', CommandsRouter);
 server.listen(3000, "0.0.0.0", () => console.log("Hello World"));
