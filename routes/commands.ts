d317d4642191290e60ccb1d01270cd2429152ceb
Unbreakablenight <unbreakableplayz23@gmail.com>
2023-04-06 14:23:06 +1000

    ⚒ - Construction

--- routes/commands.ts
+++ routes/commands.ts
@@ -1,14 +1,25 @@
 import express, { Request, Response } from "express";
-
+import http from "http";
+import { verifyString } from "discord.js"
 export const router: express.Router = express.Router();
 export enum ClientDetails {
     APP_ID = "994930207918129182",
-    GUILD_ID = ""
+    GUILD_ID = "1043318603392483358",
+
 }
 
 router.post("/interactions", (req: Request, res: Response) => {
     const { body } = req;
     const { type } = body;
+    const signature = req.headers['x-signature-ed25519'];
+    const timestamp = req.headers['x-signature-timestamp'];
+    const rawBody = req.body;
+
+    const isVerified = verifyString(rawBody);
+
+    if (!isVerified) {
+        return res.status(401).end('Invalid request signature');
+    }
 
     // Check the type of incoming payload
     if (type === 1) {
@@ -17,15 +28,37 @@ router.post("/interactions", (req: Request, res: Response) => {
         const options = {
             hostname: 'discord.com',
             port: 443,
-            path: `/api/v8/applications/${ClientDetails.APP_ID}/commands`,
+            path: `/api/v10/applications/${ClientDetails.APP_ID}/commands`,
             method: 'POST',
             headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bot ${process.env['BOT_TOKEN']}`
+                'Content-Type': 'application/json',
+                'Authorization': `Bot ${process.env['BOT_TOKEN']}`
             }
         };
-        // Handle Ping payload
-        console.log("Received Ping payload from Discord");
+
+        const request = http.request(options, response => {
+            let data = '';
+      
+            response.on('data', chunk => {
+              data += chunk;
+            });
+      
+            response.on('end', () => {
+              console.log(data);
+            });
+        });
+
+        const command = {
+            name: "ping",
+            description: 'Pong!'
+          };
+      
+          const requestData = JSON.stringify(command);
+      
+          request.write(requestData);
+          request.end();
+      
+          res.status(200).end();
         res.status(200).json({ type: 1 });
     } else if (type === 2) {
         // Handle ApplicationCommand payload
